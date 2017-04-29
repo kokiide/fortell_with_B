aa855bb71177eea7af85b66f480e39254192b8a4
Koki.Ide <s15087ki@sfc.keio.ac.jp>
2017-04-29 11:35:32 +0900

    Initial Commit

---
+++ Fortell_with_B/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fortell_with_B
+//
+//  Created by Koki Ide on 2017/04/29.
+//  Copyright © 2017 kokiide. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
